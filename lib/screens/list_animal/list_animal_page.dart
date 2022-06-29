f89bda3dca8d5f779597a7bb54ec6a37bc82d02c
eliander1 <camposeliander@gmail.com>
2022-06-29 08:17:28 -0400

    pasta constants adicionada

--- lib/screens/list_animal/list_animal_page.dart
+++ lib/screens/list_animal/list_animal_page.dart
@@ -3,26 +3,22 @@ import 'package:app_ong/screens/details/details_page.dart';
 import 'package:app_ong/screens/list_animal/list_animal_controller.dart';
 import 'package:flutter/material.dart';
 
-
-
 class ListAnimalPage extends StatefulWidget {
   String animal;
-   ListAnimalPage({Key? key,required this.animal}) : super(key: key);
+
+  ListAnimalPage({Key? key, required this.animal}) : super(key: key);
+
   @override
   State<ListAnimalPage> createState() => _ListAnimalPageState();
 }
-
 class _ListAnimalPageState extends State<ListAnimalPage> {
-  ListAnimalController listAnimalController = ListAnimalController(); 
-
-
+  ListAnimalController listAnimalController = ListAnimalController();
 
   @override
   void initState() {
     super.initState();
-      listAnimalController.populateList(widget.animal);
+    listAnimalController.populateList(widget.animal);
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -39,20 +35,25 @@ class _ListAnimalPageState extends State<ListAnimalPage> {
                 return Card(
                   elevation: 5,
                   child: ListTile(
-                    title:dogData[index].name != null ? Text(dogData[index].name!):Text('Sem nome'),
-                    leading:dogData[index].image != null ? dogData[index].image!.url != null ?Image.network(
-                            dogData[index].image!.url!,
-                            height: 100,
-                            width: 100,
-                            fit: BoxFit.cover,
-                          ):Icon(Icons.image): Icon(Icons.image),
+                    title: dogData[index].name != null
+                        ? Text(dogData[index].name!)
+                        : Text('Sem nome'),
+                    leading: dogData[index].image != null
+                        ? dogData[index].image!.url != null
+                            ? Image.network(
+                                dogData[index].image!.url!,
+                                height: 100,
+                                width: 100,
+                                fit: BoxFit.cover,
+                              )
+                            : Icon(Icons.image)
+                        : Icon(Icons.image),
                     onTap: () => showDetails(context, dogData[index]),
                   ),
                 );
               },
             );
           } else if (snapshot.hasError) {
-
             return Center(
               child: Text('Error: ${snapshot.error}, ${snapshot.stackTrace}'),
             );
@@ -62,20 +63,15 @@ class _ListAnimalPageState extends State<ListAnimalPage> {
         });
   }
 
-
-
   void showDetails(BuildContext context, Animal dogData) {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (context) {
           return DetailsPage(
-              animal: dogData,
-             );
+            animal: dogData,
+          );
         },
       ),
     );
-
   }
 }
-
-
